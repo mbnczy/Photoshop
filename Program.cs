3f3addadaf77700120f8d4ce3eb413cc7a84d3e0
banoczymartin <banoczy.martin@stud.uni-obuda.hu>
2023-10-01 17:59:18 +0200

    program.cs updated

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+
+app.UseRouting();
+app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
-
